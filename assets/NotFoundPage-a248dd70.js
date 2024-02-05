f44999b29c73ccba8da03fbc0c2d8f378b7089be
Hikmet Can <hiko.kufte@gmail.com>
2024-02-05 19:10:03 +0100

    fix: footer and other fixes

---
+++ assets/NotFoundPage-a248dd70.js
@@ -0,0 +1,1 @@
+import{_ as t,r as n,o as a,c as s,f as c,w as r,a as e,h as _}from"./index-5e329aed.js";const l={},d=e("h1",null,"404",-1),u=e("p",null,"Page not Found",-1);function i(f,m){const o=n("RouterLink");return a(),s("main",null,[d,u,c(o,{to:"/"},{default:r(()=>[_("Go to home")]),_:1})])}const p=t(l,[["render",i]]);export{p as default};
